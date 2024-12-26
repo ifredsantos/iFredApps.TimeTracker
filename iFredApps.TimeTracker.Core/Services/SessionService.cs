5b748167cee0b44415d44f9ed01e2430828afb83
Frederico Santos <fred.santos@outlook.pt>
2024-12-26 18:23:39 +0000

    Improvement in Session response API

--- iFredApps.TimeTracker.Core/Services/SessionService.cs
+++ iFredApps.TimeTracker.Core/Services/SessionService.cs
@@ -13,24 +13,25 @@ namespace iFredApps.TimeTracker.Core.Services
          _sessionRepository = sessionRepository;
       }
 
-      public Task<IEnumerable<Session>> GetUserSessions(int user_id, int workspace_id)
+      public async Task<Result<IEnumerable<Session>>> GetUserSessions(int user_id, int workspace_id)
       {
-         return _sessionRepository.GetUserSessions(user_id, workspace_id);
+         return Result<IEnumerable<Session>>.Ok(await _sessionRepository.GetUserSessions(user_id, workspace_id));
       }
 
-      public async Task<Session> Create(Session session)
+      public async Task<Result<Session>> Create(Session session)
       {
-         return await _sessionRepository.Create(session);
+         return Result<Session>.Ok(await _sessionRepository.Create(session));
       }
 
-      public async Task<Session> Update(Session session)
+      public async Task<Result<Session>> Update(Session session)
       {
-         return await _sessionRepository.Update(session);
+         return Result<Session>.Ok(await _sessionRepository.Update(session));
       }
 
-      public async Task Delete(int session_id)
+      public async Task<Result<bool>> Delete(int session_id)
       {
          await _sessionRepository.Delete(session_id);
+         return Result<bool>.Ok(true);
       }
    }
 }
